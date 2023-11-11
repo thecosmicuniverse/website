a5898130a60d1e474d2af2f8f8d1ec92c44a0b53
Nicholas St. Germain <nick@cajun.pro>
2023-11-10 20:18:24 -0600

    fix navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,8 +1,7 @@
 // noinspection HtmlUnknownTarget
 "use client"
 
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
-import { useTheme } from "next-themes"
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 import { Button } from "@/components/ui/button"
 import {
@@ -18,34 +17,6 @@ import Logo from "@/app/icon.png";
 import DoCMap from "@/assets/doc/map.png";
 import GenericElf from "@/assets/collections/elves/4286.png";
 
-export function ModeToggle() {
-  const {setTheme} = useTheme()
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
-          <MoonIcon
-            className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
-}
-
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -54,8 +25,8 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import { Icons } from "@/components/icons"
+} from "@/components/ui/navigation-menu";
+
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
@@ -99,7 +70,7 @@ const components: { title: string; href: string; description: string }[] = [
 
 function MainNav() {
   return (
-    <NavigationMenu>
+    <NavigationMenu className="hidden sm:flex">
       <NavigationMenuList>
         <NavigationMenuItem>
           <NavigationMenuTrigger>Play Now</NavigationMenuTrigger>
@@ -163,10 +134,17 @@ function MainNav() {
 
 function NavHome(){
   return (
-    <Link href="/" className="flex space-x-2">
-      <Image src={Logo} alt="Cosmic Universe Logo" className="w-6 h-6" />
-      <span>Cosmic Universe</span>
-    </Link>
+    <div className="flex items-center">
+      <button className="sm:hidden pr-4">
+        <HamburgerMenuIcon className="w-8 h-8" />
+      </button>
+      <Link href="/">
+        <div className="flex space-x-2 items-center">
+          <Image src={Logo} alt="Cosmic Universe Logo" className="w-6 h-6" />
+          <span className="hidden sm:flex leading-none">Cosmic Universe</span>
+        </div>
+      </Link>
+    </div>
   )
 }
 
